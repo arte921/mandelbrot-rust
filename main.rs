0e0fe5ce6d1d45d511fbc18484ed100c0bbb2530
Ard-Jan van Etten <ardjanve@gmail.com>
2020-10-06 18:57:29 +0200

    work

--- main.rs
+++ main.rs
@@ -1,16 +1,19 @@
 use pixel_canvas::{Canvas, Color};
 
-const RMIN: f64 = -2.0;
-const RMAX: f64 = 1.0;
+const RMIN: f64 = -1.7;
+const RMAX: f64 = 0.7;
 
-const IMIN: f64 = -2.0;
-const IMAX: f64 = 2.0;
+const IMIN: f64 = -1.3;
+const IMAX: f64 = 1.3;
+
+const ITERATIONS: i32 = 1000;
+const COLORFACTOR: i32 = 500;
 
 const RWIDTH: f64 = RMAX - RMIN;
 const IWIDTH: f64 = IMAX - IMIN;
 
 fn main () {
-    let canvas = Canvas::new(800, 800)
+    let canvas = Canvas::new(1000, 1000)
         .title("mandelbrot");
 
     canvas.render(|_, image| {
@@ -25,7 +28,7 @@ fn main () {
                 let gs = if isinset.0 {
                     0
                 } else {
-                    (isinset.1 as f64 / 1000.0).sqrt() as u8
+                    (((ITERATIONS - isinset.1) as f64 / COLORFACTOR as f64).sqrt() * 255.0 ) as u8
                 } as u8;
                 *pixel = Color {
                     r: gs,
@@ -39,7 +42,7 @@ fn main () {
 }
 
 fn inset (r: f64, i: f64) -> (bool, i32) {
-    mandelbrot(0.0, 0.0, r, i, 1000)
+    mandelbrot(0.0, 0.0, r, i, ITERATIONS)
 }
 
 fn mandelbrot (p: f64, q: f64, a: f64, b: f64, i: i32) -> (bool, i32) {
@@ -49,7 +52,7 @@ fn mandelbrot (p: f64, q: f64, a: f64, b: f64, i: i32) -> (bool, i32) {
     if infinite(p, q) {
         return (false, i);
     }
-    mandelbrot(a + p.powf(2.0), 2.0 * p * q + b, a, b, i - 1)
+    mandelbrot(a + p.powf(2.0) - q.powf(2.0), 2.0 * p * q + b, a, b, i - 1)
 }
 
 // checks if absolute value < 2 it wanders off to infinity
